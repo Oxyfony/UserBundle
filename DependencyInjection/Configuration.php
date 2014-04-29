e66be6785dc0303c1373a4aa4199e6bf621ee42c
Artscore Studio <info@artscore-studio.fr>
2014-04-29 08:22:55 +0200

    Enable/disable email as username in bundle configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -30,7 +30,11 @@ class Configuration implements ConfigurationInterface
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
-
+		$rootNode
+			->children()
+				->booleanNode('email_as_username')->defaultFalse()->end()
+			->end();
+        
         return $treeBuilder;
     }
 }
--- DependencyInjection/O2UserExtension.php
+++ DependencyInjection/O2UserExtension.php
@@ -13,13 +13,14 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class O2UserExtension extends Extension
+class O2UserExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -30,7 +31,49 @@ class O2UserExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 		
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services/forms.xml');
+        
+        if ( isset($config['email_as_username']) && true === $config['email_as_username'] ) {
+        	$loader->load('services/email_as_username.xml');
+        }
+        
         $loader->load('services.xml');
+    }
+    
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+    	// Get Application bundles list
+    	$bundles = $container->getParameter('kernel.bundles');
+    	
+    	// Get O2UserBundle Configuration
+    	$configuration = new Configuration();
+    	$configs = $container->getExtensionConfig($this->getAlias());
+    	$config = $this->processConfiguration($configuration, $configs);
+    	
+    	// If email as username is defined, update FOSUserBundle Configuration
+    	if ( isset($config['email_as_username']) && true === $config['email_as_username'] ) {
+    		$config = array(
+    			'registration' => array(
+    				'form' => array(
+    					'type' => 'o2_user_registration',
+    					'validation_groups' => array('o2UserRegistration')
+    				)
+    			),
+				'profile' => array(
+					'form' => array(
+						'type' => 'o2_user_profile',
+						'validation_groups' => array('o2UserProfile')
+					)
+				)
+    		);
+    		
+    		foreach($container->getExtensions() as $name => $extension) {
+    			if ( 'fos_user' === $name ) {
+    				$container->prependExtensionConfig($name, $config);
+    			}
+    		}
+    	}
     }
 }
